7399c98083d288b1b882dee2fbc9519d921615f5
Yuan Mai <4registration@gmail.com>
2017-06-01 23:06:53 +0800

    branching shell

---
+++ dont-run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git checkout master -b team-a
+
+git checkout master -b team-b
+
+git checkout master -b team-c
+
+git checkout master -b team-d
+
+git checkout master -b team-e
+
+git checkout master -b team-f
+
+git checkout master -b team-g
+
+git checkout master -b team-h
