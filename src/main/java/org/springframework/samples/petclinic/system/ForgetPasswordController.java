95e207a3be2f892ae1c796ed0f80c610520ee56e
jlcoa <jlcoa.ke@gmail.com>
2017-06-02 15:42:06 +0800

    Merge branch 'team-a' of https://github.com/yuanmai/spring-petclinic into team-a
    # Conflicts:
    #	src/features/login/login.feature
    #	src/test/java/org/springframework/samples/petclinic/PetFeatureSteps.java

--- src/main/java/org/springframework/samples/petclinic/system/ForgetPasswordController.java
+++ src/main/java/org/springframework/samples/petclinic/system/ForgetPasswordController.java
@@ -1,10 +1,12 @@
 package org.springframework.samples.petclinic.system;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by jared.li on 6/2/17.
  */
+@Controller
 public class ForgetPasswordController {
     @RequestMapping("/forgetPassword")
     public String forgetPassword() {
