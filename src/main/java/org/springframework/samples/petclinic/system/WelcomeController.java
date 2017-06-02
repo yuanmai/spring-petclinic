4edb4041ff58567f256df5299733b64dacbfa447
Raymond <raymondwuxq@gmail.com>
2017-06-02 15:38:50 +0800

    login page implement

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -3,12 +3,28 @@ package org.springframework.samples.petclinic.system;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 class WelcomeController {
+
+    static Map<String, String> map = new HashMap<>();
+
+    {
+
+    }
 
     @RequestMapping("/")
     public String welcome() {
         return "login/login";
     }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Map<String, Object> model) {
+        return "redirect:/vets.html";
+    }
+
 }
