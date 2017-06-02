2c63b175d23938a71ffb3126e8c603f92fbca6e3
marcusrc <marcus.chen@ringcentral.com>
2017-06-02 16:19:20 +0800

    Replace cucumber runner

--- src/test/java/org/springframework/samples/petclinic/CucumberAcceptanceTests.java
+++ src/test/java/org/springframework/samples/petclinic/CucumberAcceptanceTests.java
@@ -1,10 +1,9 @@
 package org.springframework.samples.petclinic;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+@RunWith(PassiveCucumber.class)
 @CucumberOptions(features = "src/features/login")
 public class CucumberAcceptanceTests {
 
