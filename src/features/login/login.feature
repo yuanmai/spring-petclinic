d3a9982930fd99cf4ca5591857ed3383ac45041e
Jared Li <jared.li@ringcentral.com>
2017-06-02 10:32:55 +0800

    TC: Login again without logout need password

--- src/features/login/login.feature
+++ src/features/login/login.feature
@@ -4,3 +4,8 @@ Feature: Login
     Given user has valid account
     When use login with correct username and password
     Then user can access the home page
+
+   Scenario: Need login when reopen app
+     Given user close app directly witout logout
+     When open app again
+     Then need to input password again
--- src/test/java/org/springframework/samples/petclinic/PetFeatureSteps.java
+++ src/test/java/org/springframework/samples/petclinic/PetFeatureSteps.java
@@ -1,5 +1,6 @@
 package org.springframework.samples.petclinic;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -31,4 +32,21 @@ public class PetFeatureSteps {
     public void user_can_access_the_home_page() throws Throwable {
     }
 
+    @Given("^user close app directly witout logout$")
+    public void user_close_app_directly_witout_logout() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^open app again$")
+    public void open_app_again() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^need to input password again$")
+    public void need_to_input_password_again() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
 }
