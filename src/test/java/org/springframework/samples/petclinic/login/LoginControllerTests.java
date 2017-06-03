2b763c5ab52235d4fa58b35ba3621187a9a3d1e1
Tony Lin <tony.lin@ringcentral.com>
2017-06-03 09:40:30 +0800

    Add LoginControllerTests.

---
+++ src/test/java/org/springframework/samples/petclinic/login/LoginControllerTests.java
@@ -0,0 +1,53 @@
+package org.springframework.samples.petclinic.login;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+/**
+ * Test class for {@link LoginController}
+ *
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(LoginController.class)
+public class LoginControllerTests {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void testRenderLogin() throws Exception {
+        mockMvc.perform(get("/login"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("login/first_form"));
+    }
+
+    @Test
+    public void testRenderEmailLogin() throws Exception {
+        mockMvc.perform(get("/login/email"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("login/email_login"));
+    }
+
+    @Test
+    public void testRenderPhoneNumberLogin() throws Exception {
+        mockMvc.perform(get("/login/phone_number"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("login/phone_number_login"));
+    }
+}
