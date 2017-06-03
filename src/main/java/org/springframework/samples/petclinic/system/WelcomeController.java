819b946d369ef5e622096ed4a4701ff09d0a0746
Raymond <raymondwuxq@gmail.com>
2017-06-03 09:16:37 +0800

    login page implement

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -1,7 +1,11 @@
 package org.springframework.samples.petclinic.system;
 
 
+import org.springframework.samples.petclinic.owner.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,20 +15,30 @@ import java.util.Map;
 @Controller
 class WelcomeController {
 
+    static boolean isLogin = false;
     static Map<String, String> map = new HashMap<>();
 
     {
-
+        map.put("admin", "admin123");
     }
 
     @RequestMapping("/")
-    public String welcome() {
+    public String welcome(Map<String, Object> model) {
+        model.put("isLogin", isLogin);
+        model.put("userName", "Welcome admin");
         return "login/login";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(Map<String, Object> model) {
-        return "redirect:/vets.html";
+    public String login(User user, Map<String, Object> model) {
+        if (map.containsKey(user.getUserName()) && map.get(user.getUserName()).equals(user.getPassword())) {
+            isLogin = true;
+            return "redirect:/vets.html";
+        } else {
+            model.put("hasError", 1);
+            model.put("error", "Incorrect userName or password.");
+        }
+        return "login/login";
     }
 
 }
