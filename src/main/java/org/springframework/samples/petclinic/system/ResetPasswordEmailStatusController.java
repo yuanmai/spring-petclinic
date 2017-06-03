ad9be380f3003a570f76f30dbe775e82b856665c
Jared Li <jared.li@ringcentral.com>
2017-06-03 13:30:18 +0800

    UI refactory

--- src/main/java/org/springframework/samples/petclinic/system/ResetPasswordEmailStatusController.java
+++ src/main/java/org/springframework/samples/petclinic/system/ResetPasswordEmailStatusController.java
@@ -1,8 +1,10 @@
 package org.springframework.samples.petclinic.system;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,21 +14,27 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class ResetPasswordEmailStatusController {
 
-    private PhoneNumber phoneNumber;
-
-    @RequestMapping("/resetPasswordEmailStatus")
-    public String resetPassword(HttpServletRequest request) {
-        phoneNumber = new PhoneNumber();
-        phoneNumber.setPhoneNumber(request.getParameter("phoneNumber"));
-        phoneNumber.setExtensionNumber(request.getParameter("extensionNumber"));
-
-        System.out.println(phoneNumber.getPhoneNumber());
-        System.out.println(phoneNumber.getExtensionNumber());
+    @RequestMapping(value = "/resetPasswordEmailStatus", method = RequestMethod.GET)
+    public String resetPasswordEmailStatus(@ModelAttribute("phoneNumber") PhoneNumber phoneNumber, Model model) {
+        /*
+         * Validate phone number and extension number
+         * Get email addr
+         * Send email
+         */
+        Result result = new Result();
+        result.setMessage(Result.MSG_SUCCESS);
+        result.setKlass(Result.STYLE_SUCCESS);
+        model.addAttribute(result);
         return "resetPasswordEmailStatus";
     }
 
     @ModelAttribute("phoneNumber")
     public PhoneNumber getPhoneNumber(){
-        return phoneNumber;
+        return new PhoneNumber();
     }
+
+//    @ModelAttribute("result")
+//    public Result getResult() {
+//        return new Result();
+//    }
 }
---
+++ src/main/java/org/springframework/samples/petclinic/system/Result.java
@@ -0,0 +1,37 @@
+package org.springframework.samples.petclinic.system;
+
+import javax.persistence.Column;
+
+/**
+ * Created by jared.li on 6/3/17.
+ */
+public class Result {
+    public static String STYLE_SUCCESS = "alert  alert-success";
+    public static String STYLE_ERROR = "alert  alert-error";
+
+    public static String MSG_SUCCESS = "A reset password email has been sent to your mailbox. Please check.";
+    public static String MSG_ERROR = "Send email error.";
+
+
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "klass")
+    private String klass;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getKlass() {
+        return klass;
+    }
+
+    public void setKlass(String klass) {
+        this.klass = klass;
+    }
+}
